1e2b30151faf805aa83e93a60346f42c917743d3
Alex Hinds <alex.hinds141@gmail.com>
2017-03-15 18:19:46 +1100

    minifier.py with some basic additions to framework. Main engine still not implemented.

--- minifier/minifier.py
+++ minifier/minifier.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+
+import sys
+
+def arg_handling():
+    for arg in sys.argv[1:]:
+        try:
+            f = open(arg, 'r')
+        except OSError:
+            print('cannot open', arg)
+        else:
+            print(arg, 'has', len(f.readlines()), 'lines')
+            parse(f,arg)
+            f.close()
+
+def parse(file_handle, fname):
+    """Main handling of the minify parse.
+    """
+    output = []
+    print_output(fname, output)
+
+def print_output(fname, output):
+    """Prints output to the same filename with a c bad extension.
+    Thus test.c reads out as test.ccbad etc."""
+    try:
+        with open(fname + "cbad",'w') as fw:
+            fw.write("STUFF")
+    except OSError:
+        print('cannot create file', fname + "cbad")
+
+if __name__ == '__main__':
+    arg_handling()
+    print("OK.")
