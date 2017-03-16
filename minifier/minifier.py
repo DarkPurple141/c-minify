c85683e25780ecb8e3039376941d40456283798b
Alex Hinds <alex.hinds141@gmail.com>
2017-03-16 21:32:03 +1100

    some progress. not passing initial tests yet. implemented basic parsing with a few obvious exceptions.

--- minifier/minifier.py
+++ minifier/minifier.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, random
+
+reserve = [
+    'auto',	'else',	'long',	'switch',
+    'break', 'enum', 'register', 'typedef',
+    'case',	'extern', 'return',	'union',
+    'char',	'float', 'short', 'unsigned',
+    'const', 'for', 'signed', 'void',
+    'continue', 'goto', 'sizeof', 'volatile',
+    'default', 'if', 'static', 'while',
+    'do', 'int', 'struct', 'double', '#include', '#define'
+]
 
 def arg_handling():
     for arg in sys.argv[1:]:
@@ -9,14 +20,33 @@ def arg_handling():
         except OSError:
             print('cannot open', arg)
         else:
-            print(arg, 'has', len(f.readlines()), 'lines')
             parse(f,arg)
             f.close()
 
 def parse(file_handle, fname):
-    """Main handling of the minify parse.
-    """
+    """Main handling of the minify parse."""
+    var = {}
     output = []
+    for line in file_handle:
+        line = line.strip()
+        if line.startswith('#define') or line.startswith('#include'):
+            output.append(line)
+            output.append('\n')
+            continue
+        elif line.startswith('//'):
+            continue
+        words = line.split()
+        for word in words:
+            if word in reserve:
+                output.append(word)
+            else:
+                try:
+                    new = var[word]
+                except KeyError:
+                    var[word] = word[0:2]
+                    new = var[word]
+                output.append(new)
+    output = " ".join(output)
     print_output(fname, output)
 
 def print_output(fname, output):
@@ -24,7 +54,7 @@ def print_output(fname, output):
     Thus test.c reads out as test.ccbad etc."""
     try:
         with open(fname + "cbad",'w') as fw:
-            fw.write("STUFF")
+            fw.write(output)
     except OSError:
         print('cannot create file', fname + "cbad")
 
